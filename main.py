b54622cd330adebe6274cd04a6b7563a11a67edf
Guilhermwn <limaoplaykglp@gmail.com>
2024-06-30 17:24:12 -0300

    update content

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+app.mount("/assets", StaticFiles(directory="assets"), name="assets")
 
 templates = Jinja2Templates(directory='templates')
 
@@ -13,4 +15,4 @@ def home(request: Request):
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app)
+    uvicorn.run(app, host='192.168.1.10')
