9663d1e9e399ae46f69a06c4ce13c00f5bc4575c
Guilhermwn <limaoplaykglp@gmail.com>
2024-07-08 12:53:26 -0300

    Refactor project to use the Franken-UI CSS framework

--- main.py
+++ main.py
@@ -1,32 +1,42 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from pathlib import Path
+
+favicon_path = 'favicon.ico'
+BASE_DIR = Path(__file__).parent
 
 app = FastAPI()
-app.mount("/assets", StaticFiles(directory="assets"), name="assets")
+app.mount("/assets", StaticFiles(directory=BASE_DIR/"assets"), name="assets")
 
 templates = Jinja2Templates(directory='templates')
 
+
+def template_response(html: str, request: Request, **kwargs):
+    context = {"request": request}
+    context.update(kwargs)
+    return templates.TemplateResponse(html, context)
+
+@app.get('/favicon.ico', include_in_schema=False)
+async def favicon():
+    return FileResponse(favicon_path)
+
 @app.get('/', response_class=HTMLResponse)
 def home(request: Request):
-    project_list = {
-        'projeto1': {
-            'image': 'image.png',
-            'title': 'Projeto',
-            'link': '/',
-            'description': 'A brief description of the project'
-        }
+    pages = {
+        'Links': '/links',
+        'Projetos': '/projetos',
+        'Sobre': '/sobre'
     }
-
-    context = {
-        'request': request,
-        'project_list': project_list
-        }
-    return templates.TemplateResponse('index.jinja', context)
-
+    return template_response(
+        'home.html', 
+        request=request, 
+        pages=pages, 
+        title='Home'
+    )
 
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host='192.168.1.10')
+    uvicorn.run('main:app', host='192.168.1.10', reload=True)
